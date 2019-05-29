50f6fd9231eb6fd6b78b7f9428cc13d0f125d919
Jeong-Yoon Lee <jeong@uber.com>
2019-05-28 18:11:35 -0700

    add the hour of day for time_cols and number of categories for multicat_cols as features

--- staring_kit_0401/sample_code_submission/model.py
+++ staring_kit_0401/sample_code_submission/model.py
@@ -13,7 +13,7 @@ from data import LabelEncoder
 from automl import predict, train, validate
 from CONSTANT import MAIN_TABLE_NAME, CATEGORY_PREFIX, MULTI_CAT_PREFIX, NUMERICAL_PREFIX, TIME_PREFIX
 from merge import merge_table
-from preprocess import clean_df, clean_tables, feature_engineer
+from preprocess import clean_df, clean_tables
 from util import Config, log, show_dataframe, timeit
 
 
@@ -36,7 +36,7 @@ class Model:
         X = merge_table(Xs, self.config)
         clean_df(X)
 
-        self.cat_cols = sorted([c for c in X.columns if c.startswith(CATEGORY_PREFIX) or X[c].dtype == np.object])
+        self.cat_cols = sorted([c for c in X.columns if c.startswith(CATEGORY_PREFIX)])
         self.mcat_cols = sorted([c for c in X.columns if c.startswith(MULTI_CAT_PREFIX)])
         self.num_cols = sorted([c for c in X.columns if c.startswith(NUMERICAL_PREFIX)])
         self.ts_cols = sorted([c for c in X.columns if c.startswith(TIME_PREFIX)])
@@ -45,13 +45,30 @@ class Model:
         X['y_sorted'] = y
         X.sort_values(self.ts_col, inplace=True)
         y = X.y_sorted.copy()
-        X.drop(['y_sorted'] + self.ts_cols, axis=1, inplace=True)
+        X.drop('y_sorted', axis=1, inplace=True)
 
-        self.enc = LabelEncoder(min_obs=X.shape[0] * .0001)
-        X.loc[:, self.cat_cols] = self.enc.fit_transform(X[self.cat_cols])
-
-        #feature_engineer(X, self.config)
+        X = self.feature_engineer(X, train=True)
         train(X, y, self.config)
+
+    @timeit
+    def feature_engineer(self, X, y=None, train=True):
+        # Use the hour of the day as a feature
+        for col in self.ts_cols:
+            X.loc[:, col] = pd.to_datetime(X[col]).dt.hour
+
+        # Label encode categorical features
+        if train:
+            self.enc = LabelEncoder(min_obs=X.shape[0] * .0001)
+            X.loc[:, self.cat_cols] = self.enc.fit_transform(X[self.cat_cols])
+        else:
+            assert self.enc is not None
+            X.loc[:, self.cat_cols] = self.enc.transform(X[self.cat_cols])
+
+        # Use the count of categories for each observation
+        for col in self.mcat_cols:
+            X.loc[:, col] = X[col].apply(lambda x: len(x.split(',')) if pd.notnull(x) else 0)
+
+        return X
 
     @timeit
     def predict(self, X_test, time_remain):
@@ -66,10 +83,7 @@ class Model:
         X = merge_table(Xs, self.config)
         clean_df(X)
 
-        X.loc[:, self.cat_cols] = self.enc.transform(X[self.cat_cols])
-        X.drop(self.ts_cols, axis=1, inplace=True)
-
-        #feature_engineer(X, self.config)
+        X = self.feature_engineer(X, train=False)
         X = X[X.index.str.startswith("test")]
         X.index = X.index.map(lambda x: int(x.split('_')[1]))
         X.sort_index(inplace=True)
